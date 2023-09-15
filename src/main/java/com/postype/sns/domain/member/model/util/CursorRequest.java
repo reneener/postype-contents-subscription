9b9fe759a33a8e5e47f18c8ace73714ad0813f93
yooyouny <jhjin2u@hanmail.com>
2023-09-15 23:26:17 +0900

    refactor: 레코드 구조 클래스로 전환

--- src/main/java/com/postype/sns/domain/member/model/util/CursorRequest.java
+++ src/main/java/com/postype/sns/domain/member/model/util/CursorRequest.java
@@ -1,17 +1,20 @@
 package com.postype.sns.domain.member.model.util;
 
-public record CursorRequest(
-	Long key, int size
-) {
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+@Getter
+@RequiredArgsConstructor
+public class CursorRequest{
+	private final Long key;
+	private final int size;
+
 	public static final Long DEFAULT_KEY = -1L;
 
 	public Boolean hasKey(){
-		return key != null;
+		return this.key != null;
 	}
 
 	public CursorRequest next(Long key){
 		return new CursorRequest(key, size);
 	}
-
-
 }
--- src/main/java/com/postype/sns/domain/member/model/util/PageCursor.java
+++ src/main/java/com/postype/sns/domain/member/model/util/PageCursor.java
@@ -1,11 +1,13 @@
 package com.postype.sns.domain.member.model.util;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 import java.util.List;
-import org.springframework.data.domain.Page;
-
-public record PageCursor<T> (
-	CursorRequest nextCursorRequest,
-	List<T> contents
-){
-
+@Getter
+@RequiredArgsConstructor
+public class PageCursor<T>{
+	private final CursorRequest nextCursorRequest;
+	private final List<T> contents;
 }
