6b59da6a6878311c164961c64e2fa5e1720766df
yooyouny <jhjin2u@hanmail.com>
2023-11-03 13:36:52 +0900

    chore: 테스트 profile용 h2 datasource 설정

--- src/test/java/com/postype/sns/SnsApplicationTests.java
+++ src/test/java/com/postype/sns/SnsApplicationTests.java
@@ -2,12 +2,14 @@ package com.postype.sns;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SnsApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	//@Test
+	//void contextLoads() {
+	//}
 
 }
