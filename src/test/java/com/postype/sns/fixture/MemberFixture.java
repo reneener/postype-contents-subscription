47dc5bb2018914f8c12bd23470b815b289bf20f3
yooyouny <jhjin2u@hanmail.com>
2023-11-10 09:41:29 +0900

    refactor: 테스트 상황에 맞는 다양한 fixture 추가

--- src/test/java/com/postype/sns/fixture/MemberFixture.java
+++ src/test/java/com/postype/sns/fixture/MemberFixture.java
@@ -2,18 +2,35 @@ package com.postype.sns.fixture;
 
 import com.postype.sns.domain.member.domain.Member;
 import com.postype.sns.domain.member.domain.MemberRole;
-import java.sql.Timestamp;
-import java.time.Instant;
+import com.postype.sns.domain.member.dto.request.MemberLoginRequest;
+import com.postype.sns.domain.member.dto.request.MemberRegisterRequest;
 
 public class MemberFixture {
-//테스트용으로 만든 Member Entity
-	public static Member get(String memberId, String password, Long Id){
-		Member member = new Member();
-			member.setId(Id);
-			member.setMemberId(memberId);
-			member.setPassword(password);
-			member.setRole(MemberRole.USER);
-			member.setRegisteredAt(Timestamp.from(Instant.now()));
-		return member;
+	public static Member get(String memberId, String password){
+		return Member.builder()
+				.memberId(memberId)
+				.password(password)
+				.role(MemberRole.USER)
+				.build();
+	}
+	public static Member get(){
+		return Member.builder()
+				.memberId("memberId")
+				.password("password")
+				.role(MemberRole.USER)
+				.email("test@test.com")
+				.build();
+	}
+
+	public static MemberRegisterRequest getRegisterCreateRequest(){
+		return MemberRegisterRequest.builder()
+				.memberId("memberId")
+				.memberName("memberName")
+				.password("password")
+				.email("test@test.com")
+				.build();
+	}
+	public static MemberLoginRequest getLoginRequest(){
+		return new MemberLoginRequest("memberId", "password");
 	}
 }
--- src/test/java/com/postype/sns/fixture/PostFixture.java
+++ src/test/java/com/postype/sns/fixture/PostFixture.java
@@ -3,17 +3,42 @@ package com.postype.sns.fixture;
 import com.postype.sns.domain.member.domain.Member;
 import com.postype.sns.domain.order.domain.Point;
 import com.postype.sns.domain.post.domain.Post;
+import com.postype.sns.domain.post.dto.request.PostCommentRequest;
+import com.postype.sns.domain.post.dto.request.PostCreateRequest;
+import com.postype.sns.domain.post.dto.request.PostModifyRequest;
 
 public class PostFixture {
-	public static Post get(String memberId, Long postId, Long id){
-		Member member = new Member();
-		member.setId(id);
-		member.setMemberId(memberId);
-
-		Post post = new Post();
-		post.setMember(member);
-		post.setId(postId);
-		post.setPrice(new Point(2000));
-		return post;
+	public static Post get(Member member){
+		return Post.builder()
+				.title("title")
+				.body("body")
+				.member(member)
+				.price(new Point(2000))
+				.build();
+	}
+	public static Post get(){
+		return Post.builder()
+				.title("title")
+				.body("body")
+				.member(MemberFixture.get("memberId", "password"))
+				.price(new Point(2000))
+				.build();
+	}
+	public static PostModifyRequest getModifyRequest(){
+		return PostModifyRequest.builder()
+				.title("title")
+				.body("modify")
+				.price(10000)
+				.build();
+	}
+	public static PostCreateRequest getCreateRequest(){
+		return PostCreateRequest.builder()
+				.title("title")
+				.body("create")
+				.price(1000)
+				.build();
+	}
+	public static PostCommentRequest getCommentCreateRequest(){
+		return new PostCommentRequest("comment");
 	}
 }
